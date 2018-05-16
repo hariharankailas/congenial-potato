00d8590b8eba72c27e93974206f7fec02d4812f0
Ramesh, Hariharan (US - Bengaluru) <hariramesh@deloitte.com>
2018-05-16 12:13:03 +0530

    may 14 trying again-4

--- index.js
+++ index.js
@@ -25,18 +25,20 @@ var apiai = require('apiai');
 var apiapp = apiai("d92b6db3d22745a78f064f96fce19db4");
 
 //Body parser
+console.log("hello darkness my old friend");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res){
   res.send('Hello Salesforce');
 });
-
+/*
 app.post('/chat', function(req, res){
 
   var request = apiapp.textRequest(req.body.chatText, {
     sessionId: '4BGUAgBUD_1NO0WOl8UjukTbKa4C5FdnYYO25tlVNknms6medUzZrGHXsC3FH72'
   });
+
   request.on('response', function(response) {
       console.log(response);
       res.send(response);
@@ -48,7 +50,7 @@ app.post('/chat', function(req, res){
   });
   request.end();
 });
-
+*/
 app.listen(port, function(){
   console.log('server started on Port 3000...')
 });
